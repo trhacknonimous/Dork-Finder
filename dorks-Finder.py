88e42712dc90485ed5a64f479aa997ab6d7363c9
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2021-12-21 08:43:30 +0100

    Add files via upload

---
+++ dorks-Finder.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# A notice to all nerds and n00bs...
+# If you will copy the developer's work it will not make you a hacker..!
+# Respect all developers, we doing this because it's fun...
+# So please, don't copy this code.
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+os.system('clear')
+print """
+\033[1;91m
+ _____ ____  _   _    _    ____ _  ___   _  ___  _   _
+|_   _|  _ \| | | |  / \  / ___| |/ / \ | |/ _ \| \ | |
+  | | | |_) | |_| | / _ \| |   | ' /|  \| | | | |  \| |
+  | | |  _ <|  _  |/ ___ \ |___| . \| |\  | |_| | |\  |
+  |_| |_| \_\_| |_/_/   \_\____|_|\_\_| \_|\___/|_| \_|"""
+print("create \033[1;92mMr. TRHACKNON")
+time.sleep(3)
+print ""
+print ""
+
+CorrectUsername = "TRHACKNON"
+CorrectPassword = "dork"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("Tool Username : ")
+    if (username == CorrectUsername):
+    	password = raw_input("Tool Password : ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:TRHACKNON
+            os.system('xdg-open http://www.memanon.ml/')
+            
+	    time.sleep(5)
+            loop = 'false'
+        else:
+            print "\033[1;93mWrong Password"
+            os.system('xdg-open http://www.memanon.ml/')
+    else:
+        print "\033[1;94mWrong Username"
+        os.system('xdg-open http://www.memanon.ml/')
+        
+        
+os.system('clear')
+      
+        
+os.system ('python3 ...')
